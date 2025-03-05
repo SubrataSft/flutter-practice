f9a2fd76cb3439168d817f5d7d12a0e33f57cb91
Subrata <chandrasinghasubrata@gmail.com>
2025-03-05 23:02:22 +0600

    device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp( DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp(), // Wrap your app
+  ),);
 }
 
 class MyApp extends StatelessWidget {
@@ -9,7 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home(), theme: ThemeData());
+    return MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        home: Home(), theme: ThemeData());
   }
 }
 
