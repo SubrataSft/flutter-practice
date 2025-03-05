1e612925c1af3ab9d47a308a5a49666578bc753a
Subrata <chandrasinghasubrata@gmail.com>
2025-03-05 12:53:55 +0600

    etc builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/splash.dart';
 
-import 'home_screen.dart';
-
-void main(){
+void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -13,38 +9,68 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Splash(),
-    );
+    return MaterialApp(home: Home(), theme: ThemeData());
   }
 }
 
-/*class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
-
-  int counter = 0;
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size);
+    print(MediaQuery.of(context).size.height);
+    print(MediaQuery.of(context).size.width);
+    print(MediaQuery.of(context).orientation);
+    print(MediaQuery.of(context).displayFeatures);
+
+    print(MediaQuery.displayFeaturesOf(context));
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        leading: Icon(Icons.home),
-        actions: [
-          Icon(Icons.add),
-        ],
-      ),
-      body: Center(
-        child: Text(counter.toString(), style: TextStyle(
-            fontSize: 40
-        ),),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        counter++;
-        print(counter);
-      }, child: Icon(Icons.add),),
+      appBar: AppBar(backgroundColor: Colors.green, title: Text("Home")),
+      /* body: Center(
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          spacing: 5,
+          children: [
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+            Text(MediaQuery.orientationOf(context).toString()),
+          ],
+        ),
+      ),*/
+      /* body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if(constraints.maxHeight < 400){
+            return Center(child: Text("mobile"));
+          }else if(constraints.maxHeight < 600){
+            return Center(child: Text("Tablet"));
+          }else if(constraints.maxHeight < 600){
+            return Center(child: Text("Laptop"));
+          }
+          return Center(child: Text("Desktop"));
+        },
+      ),*/
+      body: OrientationBuilder(builder: (context,orientation){
+        if(orientation == Orientation.landscape){
+          return Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.red,
+          );
+        }else{
+          return Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.pink,
+          );
+        }
+      }),
     );
   }
-}*/
-
-
+}
