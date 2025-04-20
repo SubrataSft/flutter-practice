073cc97e84cc737c9e5e1b97c3bf33b26456c21a
Subrata <chandrasinghasubrata@gmail.com>
2025-04-20 16:33:38 +0600

    ios class

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,41 @@
-import 'package:flutter/material.dart';
-
-import 'home.dart';
+import 'package:flutter/cupertino.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(IOSStyle());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class IOSStyle extends StatelessWidget {
+  const IOSStyle({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Home(),
+    return CupertinoApp(
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text("Hello Ios Phone"),
+          trailing: CupertinoButton.filled(child: Icon(CupertinoIcons.add), onPressed: (){}),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CupertinoActivityIndicator(
+                    radius: 24,
+                  ),
+                  CupertinoSwitch(value: false, onChanged: (value){})
+                ],
+              ),
+        ));
   }
 }
 
