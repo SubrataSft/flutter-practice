67a7b9b446a6f33e9a5c0bc3f1cd4a450ceac3d1
Subrata <chandrasinghasubrata@gmail.com>
2025-04-21 22:00:07 +0600

    Extra device

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        home: HomeScreen());
   }
 }
 
